b15ac2e7b9a559f2c00fe5e6d08b1ecabae9fe0e
mbuso <mbsmbokwana@gmail.com>
2022-08-17 23:21:01 +0200

    task 0.7

--- task07.py
+++ task07.py
@@ -1,15 +1,9 @@
-celsius = int(28)
-
 def convert(c):
-    return (9/5 * c) + 32  #your code goes here
+    return (9/5 * c) + 32  
     
-fahrenheit = convert(celsius)
-print(fahrenheit)
-
-fahrenheit = 82.4
+fahrenheit = print(convert(28))
 
 def opp(f):
     return (f - 32) * (5/9)
 
-transmute = opp(fahrenheit)
-print(transmute)
+transmute = print(opp(82.4))
