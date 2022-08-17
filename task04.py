af15b9a74971c95fadc0328222f3c0599c5a9985
mbuso <mbsmbokwana@gmail.com>
2022-08-17 23:18:43 +0200

    task 0.4

--- task04.py
+++ task04.py
@@ -1,9 +1,7 @@
-
-y = int()
-
-def even_or_odd(y):
-    if y%2==0:
+def even_or_odd(arg):
+    if arg%2==0:
         print("even")
     else:
         print("odd")
-print(even_or_odd(7))
+
+print(even_or_odd(6))
