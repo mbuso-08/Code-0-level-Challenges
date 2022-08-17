67643dd28a1ce910fdb2f0198cf56a62eaad208f
mbuso <mbsmbokwana@gmail.com>
2022-08-17 23:22:22 +0200

    task 0.10

--- task10.py
+++ task10.py
@@ -1,12 +1,13 @@
-texts="mbuso" 
-contexts="mbokwana" 
-result=[] 
-if (len(texts) < len(contexts)): 
-	for l in texts: 
-		if(l in contexts): 
-			result.append(l) 
-else: 
-	for l in contexts: 
-		if(l in texts): 
-			result.append(l) 
-print("Common letters:", result)
+result= []
+def common_letters(texts, contexts):
+	if (len(texts) < len(contexts)): 
+		for l in texts: 
+			if(l in contexts): 
+				result.append(l) 
+	else: 
+		for l in contexts: 
+			if(l in texts): 
+				result.append(l) 
+	print("Common letters:", result)
+
+print(common_letters("Mbuso", "Mbokwana"))
