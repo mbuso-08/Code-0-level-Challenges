1e31753589466c1f243f8dfe590969c29431aa24
mbuso <mbsmbokwana@gmail.com>
2022-08-17 23:21:24 +0200

    task 0.8

--- task08.py
+++ task08.py
@@ -1,7 +1,6 @@
-time=133
+def time_func(t):
+    hours = int(t) // 60
+    minutes = (t % 60 ) % 60 
+    print("%d:%02d" % (hours, minutes))
 
-hours = int(time) // 60
-minutes = (time % 60 ) % 60 
-
-print("%d:%02d" % (hours, minutes))
-
+print(time_func(133))
