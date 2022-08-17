8f0d520948d7c53d1e6234e48bcdb579e4775b34
mbuso <mbsmbokwana@gmail.com>
2022-08-17 23:21:48 +0200

    task 0.9

--- task09.py
+++ task09.py
@@ -1,6 +1,10 @@
 def vowel_function(text):
-    vowels = "AEIOUaeiou" 
-    lis = [letter for letter in text if letter in vowels]
+    vowels = ("AEIOUaeiou") 
+    lis = list()
+    for letter in text:
+        l = letter.lower()
+        if ( l in vowels ) and (l not in lis ):
+            lis.append(l)
     print(", ".join(lis))
 
 vowel_function("Umuzi")
